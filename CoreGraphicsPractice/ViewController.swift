988e29d757acbd8436d1ccd1abca2caf3474e038
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-29 17:40:35 -0700

    Draw Square, Ellipse, Checkers

--- CoreGraphicsPractice/ViewController.swift
+++ CoreGraphicsPractice/ViewController.swift
@@ -10,11 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentDrawType = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        drawRectangle()
+    }
+    
+    func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: .init(width: 512, height: 512))
+        let image = renderer.image { (context) in
+            context.cgContext.setFillColor(UIColor.black.cgColor)
+            
+            for row in 0 ..< 8 {
+                for col in 0 ..< 8 {
+                    if (row + col) % 2 == 0 {
+                        context.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: .init(width: 512, height: 512))
+        let image = renderer.image { (context) in
+            context.cgContext.setFillColor(UIColor.red.cgColor)
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.setLineWidth(10)
+            context.cgContext.addEllipse(in: .init(x: 5, y: 5, width: 502, height: 502))
+            context.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let image = renderer.image { (context) in
+            let rectangle = CGRect(x: 5, y: 5, width: 502, height: 502)
+            
+            context.cgContext.setFillColor(UIColor.red.cgColor)
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.setLineWidth(10)
+            context.cgContext.addRect(rectangle)
+            context.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
     }
 
-
+    @IBAction func redraw(_ sender: Any) {
+        
+        currentDrawType += 1
+        
+        if currentDrawType >= 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerboard()
+        default:
+            break
+        }
+        
+    }
+    
 }
 
